2048b0f395d420fb564ed950249e80c81b9f318b
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-11-11 12:13:50 -0500

    added do not close warning
    added comments in script and a do no close warning for the watching
    process

--- stage4.bat
+++ stage4.bat
@@ -2,19 +2,26 @@
 sfc /scannow
 ping localhost -n 1 > nul
 
+title xpSFC watcher
+
 rem winlogon.exe if it is there
 rem INFO: No tasks running with the specified critera.
 
 :loop
 tasklist /fi "WINDOWTITLE eq windows file protection" | find /i "winlogon" > nul
 if ERRORLEVEL 1 (
-	echo not running?
-	echo %errorlevel%
+	rem this is what happens when it is not running
+	rem echo not running?
+	rem echo %errorlevel%
 	goto continue
 ) else (
+	rem this is what happens when it is running
+	cls
+	echo.
+	echo DO NOT CLOSE THIS WINDOW
 	echo its still running...
 	rem er0
-	echo %errorlevel%
+	rem echo %errorlevel%
 	ping localhost -n 5 > nul
 	goto loop
 )
