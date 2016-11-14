90dd419ef89660b3a2ed6614848061c44b218cc3
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-11-14 01:30:54 -0500

    added text variable for cd letter assignment
    using wincdemu /check, we get a decimal value for the drive letter
    output as errorlevel. Then we use %=exitcodeascii% to get its ascii
    representation.

--- cdmount.bat
+++ cdmount.bat
@@ -3,7 +3,7 @@ pushd %~dp0 2>NUL
 rem http://wincdemu.sysprogs.org/tutorials/cmdline/
 batchmnt winxpsp3.iso
 
-rem check drive letter, return as errorleve
+rem check drive letter, return as errorlevel
 batchmnt /check winxpsp3.iso
 rem http://www.dostips.com/forum/viewtopic.php?t=2610
 echo %=exitcodeascii%
