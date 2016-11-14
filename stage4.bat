d0dc815ffcce00a2dcfb53471f7d92bcf0d6cc00
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-11-14 10:10:04 -0500

    0.9 fully tested on windows xp
    made syntax changes and fully tested the script on windows xp

--- stage4.bat
+++ stage4.bat
@@ -2,29 +2,42 @@
 pushd %~dp0 2>NUL
 
 for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| find "ProductName"') DO set WIN_VER=%%i %%j
-if not "%WIN_VER:~0,19%"=="Windows XP" (
+if not "%WIN_VER%"=="Microsoft Windows XP" (
 	echo.
-	echo only Windows XP is supported. This is %win_ver%.
+	echo only Microsoft Windows XP is supported. This is %win_ver%.
 	echo.
 	pause
 	exit
 )
 
 rem export registry keys
-reg export "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" temp.reg
+echo make registry backup
+reg export "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" temp.reg >nul
+echo.
 
 rem install wincdemu drivers and mount image
+echo installing drivers...
 PortableWinCDEmu-4.0.exe /install
-PortableWinCDEmu-4.0.exe winxpsp3.iso rem mount
+echo.
+echo mounting image...
+PortableWinCDEmu-4.0.exe "XP Pro SP3 (32).iso" rem mount
+rem delay for four seconds for the image to mount
+ping -n 5 localhost >nul
+echo.
 
 rem get drive letter and edit registry
-PortableWinCDEmu-4.0.exe /check winxpsp3.iso
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v SourcePath /d %=exitcodeascii%: /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v ServicePackSourcePath /d %=exitcodeascii%: /f
+REM echo check drive assignment...
+PortableWinCDEmu-4.0.exe /check "XP Pro SP3 (32).iso"
+set cddrive=%=exitcodeascii%
+REM echo %=exitcodeascii%
+echo updating registry
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v SourcePath /d %cddrive%:\ /f >nul
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v ServicePackSourcePath /d %cddrive%:\ /f >nul
+REM pause
 
 rem start the scan process
 sfc /scannow
-ping localhost -n 1 > nul rem delay for one second for the process to show up in tasklist
+REM ping -n 1 localhost >nul rem delay for one second for the process to show up in tasklist
 
 title xpSFC watcher
 
@@ -37,6 +50,7 @@ if ERRORLEVEL 1 (
 	rem this is what happens when it is not running
 	rem echo not running?
 	rem echo %errorlevel%
+	cls
 	goto continue
 ) else (
 	rem this is what happens when it is running
@@ -53,6 +67,11 @@ if ERRORLEVEL 1 (
 :continue
 rem this is what happens after it runs
 rem restore the registry key we exported at the begining
-reg import temp.reg
+echo updating registry
+reg import temp.reg >nul
 PortableWinCDEmu-4.0.exe /unmountall
-pause rem debug pause
+
+rem send an email on completion
+REM SwithMail.exe /s /x "fostatek.xml"
+echo.
+pause
