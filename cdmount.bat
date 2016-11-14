befb643441c2fe12292c069801d30220e4894037
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-11-14 01:35:35 -0500

    install wincdemu drivers
    cdmount script will run batchmnt /install to install drivers first

--- cdmount.bat
+++ cdmount.bat
@@ -1,5 +1,8 @@
 @echo off
 pushd %~dp0 2>NUL
+rem need to install drivers first
+batchmnt /install
+
 rem http://wincdemu.sysprogs.org/tutorials/cmdline/
 batchmnt winxpsp3.iso
 
@@ -10,4 +13,7 @@ echo %=exitcodeascii%
 
 rem unmount
 batchmnt /unmountall
+
+rem uninstall drivers too?
+rem dont uninstall because sometimes it can fuck up if we didnt unmount
 pause
