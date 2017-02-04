1514354941441f8ded4c2916e7275d4c6cd64de4
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-02-04 03:00:52 -0500

    Windows XP part can now use extracted i386 folder. Addressed an issue where the setup window would sometimes not close. Fewer delays.

--- stage4.bat
+++ stage4.bat
@@ -1,19 +1,19 @@
 @echo off
-:: Windows XP ISO location
+:: ----------------------------------------------------------------------------
+:: USER SETTINGS - you only need to change settings for windows xp
+:: ----------------------------------------------------------------------------
+:: SFC mode [iso|folder]
+set sfc_mode=folder
+:: Windows XP ISO location (no quotes) (set this if you set the sfc mode to iso)
 set xp_iso=XP Pro SP3 (32).iso
-
+:: Windows XP i386 folder location (no quotes) (set this if you set the sfc mode to folder) (this is the root folder, not the i386 folder itself)
+set xpsource=%~dp0
+:: ----------------------------------------------------------------------------
 pushd %~dp0 2>NUL
 
 :: detect Windows Version
 for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| find "ProductName"') DO set WIN_VER=%%i %%j
 for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentVersion ^| FIND "CurrentVersion"') DO set WIN_VER_NUM=%%i
-REM if not "%WIN_VER%"=="Microsoft Windows XP" (
-	REM echo.
-	REM echo only Microsoft Windows XP is supported. This is %win_ver%.
-	REM echo.
-	REM pause
-	REM exit
-REM )
 
 :: Windows XP
 if %WIN_VER_NUM% equ 5.1 goto windowsxp
@@ -26,54 +26,92 @@ if %WIN_VER_NUM% geq 6.2 goto win8
 :: WINDOWS XP
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :windowsxp
+:: we need delayed expansion so we can recall variables in the if statement
+setlocal enabledelayedexpansion
+if %sfc_mode%==iso (
+	:: install wincdemu drivers and mount image
+	echo installing drivers...
+	PortableWinCDEmu-4.0.exe /install
+	echo.
+	echo mounting image...
+	PortableWinCDEmu-4.0.exe "%xp_iso%"
+	echo.
+	:: we wait two seconds for the image to mount
+	ping localhost -n 3 >nul
+	:: get drive letter and edit registry. We put a trailing backslash 
+	PortableWinCDEmu-4.0.exe /check "%xp_iso%"
+	set xpsource=!=exitcodeascii!:\\
+	REM echo DEBUG !xpsource!
+) else if %sfc_mode%==folder (
+	:: if the folder path as a trailing backslash we remove it so it doesnt apppear as an escape character
+	rem http://stackoverflow.com/questions/2952401/remove-trailing-slash-from-batch-file-input
+	IF %xpsource:~-1%==\ SET xpsource=%xpsource:~0,-1%
+) else (
+	echo .
+	echo Invalid SFC mode detected ^(%sfc_mode%^). Valid options are iso or folder
+	pause>nul
+	goto eof
+)
+
+REM echo DEBUG %xpsource%
+REM echo %xpsource:~0,-1%
+
+:: in this loop we wait for 6 seconds to close the autorun window, otherwise we carry on
+if %sfc_mode%==iso (
+	echo please wait...
+	set cnt2=0
+	:setup_loop
+	tasklist /fi "WINDOWTITLE eq Welcome to Microsoft Windows XP" 2>nul | find /i "SETUP.EXE" >nul
+	if ERRORLEVEL 1 (
+		:: this is what happens when it is not running
+		set /a cnt2+=1
+		if "%cnt2%" equ "6" goto :setup_continue
+		ping localhost -n 2 >nul
+		goto setup_loop
+	) else (
+		:: this is what happens when it is running
+		goto setup_continue
+	)
+	:setup_continue
+	:: this is what happens after it runs
+	:: taskill here instead of in the loop just in case its actually open or something
+	taskkill /im setup.exe /fi "WINDOWTITLE eq Welcome to Microsoft Windows XP" >nul
+)
+
 :: export registry keys
 echo make registry backup
 reg export "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" temp.reg >nul
+REM for /f "tokens=3" %%i IN ('reg query "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v SourcePath ^| find "SourcePath"') DO set s_path="%%i"
+REM for /f "tokens=3*" %%i IN ('reg query "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v ServicePackSourcePath ^| find "ServicePackSourcePath"') DO set sp_path="%%i"
+REM reg export "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" autoruntemp.reg >nul
+REM for /f "tokens=3*" %%i IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDriveTypeAutorun ^| find "NoDriveTypeAutorun"') DO set d_auto=%%i
+REM echo %s_path%
+REM echo %sp_path%
+REM echo %d_auto%
+REM pause
 echo.
-
-:: install wincdemu drivers and mount image
-echo installing drivers...
-PortableWinCDEmu-4.0.exe /install
-echo.
-echo mounting image...
-PortableWinCDEmu-4.0.exe "%xp_iso%" rem mount
-:: delay for four seconds for the image to mount
-ping -n 5 localhost >nul
-:: close the setup window if autorun opens it
-taskkill /im setup.exe /fi "WINDOWTITLE eq Welcome to Microsoft Windows XP" >nul
-echo.
-
-:: get drive letter and edit registry
-PortableWinCDEmu-4.0.exe /check "%xp_iso%"
-set cddrive=%=exitcodeascii%
-REM echo %=exitcodeascii%
 echo updating registry
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v SourcePath /d %cddrive%:\ /f >nul
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /v ServicePackSourcePath /d %cddrive%:\ /f >nul
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /f /v SourcePath /d "%xpsource%" >nul
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /f /v ServicePackSourcePath /d "%xpsource%" >nul
+endlocal
 
 :: start the scan process
 sfc /scannow
-REM ping -n 1 localhost >nul rem delay for one second for the process to show up in tasklist
 
 title xpSFC watcher
 echo.
 echo DO NOT CLOSE THIS WINDOW
 echo its still running...
 :: winlogon.exe if it is there
-:: INFO: No tasks running with the specified critera.
 :: http://stackoverflow.com/questions/8177695/how-to-wait-for-a-process-to-terminate-to-execute-another-process-in-batch-file
 :: http://stackoverflow.com/questions/162291/how-to-check-if-a-process-is-running-via-a-batch-script
 :loop
-tasklist /fi "WINDOWTITLE eq windows file protection" | find /i "winlogon" > nul
+tasklist /fi "WINDOWTITLE eq windows file protection" 2>nul | find /i "winlogon" > nul
 if ERRORLEVEL 1 (
 	:: this is what happens when it is not running
-	rem echo %errorlevel%
-	REM cls
 	goto continue
 ) else (
 	:: this is what happens when it is running
-	REM cls
-	rem echo %errorlevel%
 	ping localhost -n 6 > nul
 	goto loop
 )
@@ -84,8 +122,15 @@ if ERRORLEVEL 1 (
 echo.
 echo updating registry...
 reg import temp.reg >nul
-PortableWinCDEmu-4.0.exe /unmountall
-rem del temp.reg >nul
+REM IF %s_path:~-1%==\ SET s_path=%s_path:~0,-1%
+REM echo "%s_path%"
+REM reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /f /v SourcePath /d "%s_path%" >nul
+REM reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Setup" /f /v ServicePackSourcePath /d "%sp_path%" >nul
+REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDriveTypeAutorun /t REG_DWORD /d %d_auto% /f >nul
+
+if %sfc_mode%==iso PortableWinCDEmu-4.0.exe /unmountall
+echo.
+del temp.reg >nul
 goto checkdisk
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -123,6 +168,7 @@ goto checkdisk
 :checkdisk
 :: check disk
 :: set the drive as dirty and then we will run the check when we restart
+echo.
 fsutil dirty set %SystemDrive%
 :: chkdsk %SystemDrive%
 :: if /i not %ERRORLEVEL%==0 (
@@ -135,7 +181,5 @@ fsutil dirty set %SystemDrive%
 :: 	REM shutdown /r /f
 :: )
 
-:: send an email on completion
-REM SwithMail.exe /s /x "fostatek.xml"
 echo.
 REM pause
